518e93e8276a09b690bd35264eae2c55d3bfd0fa
Mikhail Yankelevich <mikhailyankelevich@gmail.com>
2018-04-04 15:38:58 +0100

    test

---
+++ prctice/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void)
+{
+   for( int huy= 2; huy<=6967; huy++ )//huy
+    {
+        long long int i=3;
+        for( int j=2; j<=huy; j++)
+        {
+            i*=3;
+            
+            
+           
+        }
+        i=i%19;
+     printf ("%lld\n", i );
+    }
+    return 0;
+}
+
